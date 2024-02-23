6476af04e29c44a70d94169656a6906e4850b76d
Bityta <bituckihs@gmail.com>
2024-02-24 02:59:03 +0300

    init

---
+++ src/main/java/ru/app/tasktrackerstarter/TaskTrackerStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.app.tasktrackerstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/app/tasktrackerstarter/TaskTrackerStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.app.tasktrackerstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
